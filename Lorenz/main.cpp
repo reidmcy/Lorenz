2fe496c8f3ba4ab4200b2615e7a6af6801ce3136
Reid <Reid@v1020-wn-141-153.campus-dynamic.uwaterloo.ca>
2015-02-09 13:45:24 -0500

    Intial commit, Local

---
+++ Lorenz/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lorenz
+//
+//  Created by Reid on 2015-02-09.
+//  Copyright (c) 2015 Reid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
